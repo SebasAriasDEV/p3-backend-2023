b31ad4abe158117aa32f8d7c89fd8c6651df0de9
Sebas Dev <sebasariasdev@gmail.com>
2023-05-24 19:31:25 +0200

    Initial express server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hola mundo typescript');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
