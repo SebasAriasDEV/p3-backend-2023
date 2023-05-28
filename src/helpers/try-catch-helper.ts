81c07a3a0f2a4fa15abdc80a8e35f0e69ec6c6e5
Sebas Dev <sebasariasdev@gmail.com>
2023-05-28 17:16:01 +0200

    Added error handler high level function

--- src/helpers/try-catch-helper.ts
+++ src/helpers/try-catch-helper.ts
@@ -1,5 +1,13 @@
-import { NextFunction, RequestHandler } from 'express';
- 
-const errorHandler = ( handler: RequestHandler) => {
-    
-}
+import { RequestHandler } from 'express';
+
+const errorHandler = (handler: RequestHandler): RequestHandler => {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export { errorHandler };
--- src/routers/farms-router.ts
+++ src/routers/farms-router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { createFarm } from '../controllers/farms-controller';
+import { errorHandler } from '../helpers/try-catch-helper';
 
 const router: Router = Router();
 
 //Get all animals
-router.post('/', createFarm);
+router.post('/', errorHandler(createFarm));
 
 export default router;
